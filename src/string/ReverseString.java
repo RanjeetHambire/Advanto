7231e32b45fd32922b1e4ad3f53632110f4d083f
Ranjeet Hambire <ranjeethambire96@gmail.com>
2023-09-05 01:38:59 +0530

    Reverse String

---
+++ src/string/ReverseString.java
@@ -0,0 +1,40 @@
+package string;
+
+import java.util.Scanner;
+
+public class ReverseString {
+	
+//	             first method 
+
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Enter the String: ");
+//		String str= sc.nextLine();
+//		String rev = "";
+//		char ch ;
+//		
+//		for(int i = 0; i<str.length();i++) {
+//			ch = str.charAt(i);
+//			rev = ch + rev;
+//		}
+//		System.out.println(rev);
+//
+//	}
+
+//            Second Method	
+	
+	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System.in);
+		System.out.println("Enter the String: ");
+		String str = sc.nextLine();
+		String rev = "";
+		char ch;
+		
+		for(int i= str.length() - 1; i>=0 ; i--) {
+			ch = str.charAt(i);
+			rev += ch;
+		}
+		System.out.println(rev);
+	}
+	
+}
