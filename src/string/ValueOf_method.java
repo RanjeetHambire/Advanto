509c175e7e7b29d37951db5c13f2dfed1b8c973c
Ranjeet Hambire <ranjeethambire96@gmail.com>
2023-09-05 12:42:52 +0530

    valueOf() method

---
+++ src/string/ValueOf_method.java
@@ -0,0 +1,18 @@
+package string;
+
+import java.util.Scanner;
+
+//valueOf() - it convert int to str
+
+public class ValueOf_method {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number:");
+		
+		int num = sc.nextInt();
+		String str = String.valueOf(num);	
+		System.out.println(str + 10);
+	}
+
+}
